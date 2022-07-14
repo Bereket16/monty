e828f181f9bd17c199a9d385d2367fc3539bcdd9
YE Bereket <bereketabebe392@gmail.com>
2022-07-14 18:17:12 -0300

    my commit

---
+++ m_stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * m_stack - sets the format of the data to a stack (LIFO)
+er)
+{
+	(void)stack;
+	(void)line_number;
+	var.queue * @stack: double pointer to the beginning of the stack
+ * @line_number: script line number
+ *
+ * Return: void
+ */
+void m_stack(stack_t **stack, unsigned int line_numb = STACK;
+}
